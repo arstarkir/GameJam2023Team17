6fbbaf0f4d3e75d7e68332f975b56c5ccad72bfc
arstarkir <arstarkir@gmail.com>
2023-09-29 22:22:51 -0400

    push 2
    2

---
+++ Assets/Scripts/GatePointGenerator.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GatePointGenerator : MonoBehaviour
+{
+    public GameObject mCamera;
+    public List<GameObject> Gates = new List<GameObject>();
+
+    GameObject tempGm;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void GatePointCreator()
+    {
+
+                tempGm = hit1.transform.gameObject;
+                tempUI = Instantiate<GameObject>(talkUI, hit1.point, Quaternion.identity, canvas.transform);
+                tempUI.transform.position = new Vector2(150, 50);
+
+    }
+}
